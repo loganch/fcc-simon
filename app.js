d865dc686ca75728192f9f3a4186d391d8bdbc8d
Logan Hampton <lhampton@fincoad.com>
2018-01-10 14:01:49 -0700

    Add to sequence and reset

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var app = new Vue({
     el: '#app',
 
+    data: {
+        sequence: [],
+        stepCount: 0  
+    },
+
     methods: {
         testAlert: function() {
             alert("TEST!")
+        },
+
+        addToSequence: function() {
+            var newNumber = Math.floor(Math.random() * 4) + 1
+            this.sequence.push(newNumber)
+            this.stepCount += 1
+        },
+
+        reset: function() {
+            this.sequence = []
+            this.stepCount = 0
         }
     }
 })
